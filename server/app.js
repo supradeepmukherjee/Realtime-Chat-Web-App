9c022514cb0ebeff107bd2a14b3e8472aaf55c9c
Supradeep <supradeep2004@gmail.com>
2024-04-09 22:15:27 +0530

    Started working on Socket.io server

--- server/app.js
+++ server/app.js
@@ -9,19 +9,23 @@ import admin from './routes/admin.js'
 import { connectDB } from './utils/features.js'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import { new_msg, alert } from './constants/events.js'
+import { v4 as randomId } from 'uuid'
+import { getSockets } from './lib/helper.js'
+
 import { } from './seeders/msg.js'
-import { new_msg } from './constants/events.js'
 
 dotenv.config({ path: './.env' })
 
 const port = process.env.PORT || 6000
-export const envMode = process.env.NODE_ENV.trim() || 'PRODUCTION'
+const envMode = process.env.NODE_ENV.trim() || 'PRODUCTION'
 
 const app = express()
 const server = createServer(app)
 const io = new Server(server, {
-    transports: ['websocket']
+
 })
+const userSocketIDs = new Map()
 
 app.use(express.json())
 app.use(cookieParser())
@@ -30,7 +34,7 @@ connectDB(process.env.MONGO_URI)
 
 app.use('/user', user)
 
-export const adminKey = process.env.ADMIN_KEY
+const adminKey = process.env.ADMIN_KEY
 app.use('/admin', admin)
 
 app.use(isAuthenticated)
@@ -38,8 +42,32 @@ app.use('/chat', chat)
 
 io.on('connection', socket => {
     console.log('user connected', socket.id)
-    socket.on(new_msg,()=>{})
+    userSocketIDs.set('user._id.toString()', socket.id)
+    socket.on(new_msg, async ({ id, members, msg }) => {
+        const realTimeMsg = {
+            msg,
+            id: randomId(),
+            chat: new Date().toISOString(),
+            createdAt: id,
+            sender: {
+                _id: 'user._id',
+                name: 'user.name'
+            }
+        }
+        const dbMsg = {
+            content: msg,
+            chat: id,
+            sender: 'user._id'
+        }
+        const membersSocket = getSockets(members)
+        io.to(membersSocket).emit(new_msg, {
+            id,
+            msg: realTimeMsg
+        })
+        io.to(membersSocket).emit(alert, { id })
+    })
     socket.on('disconnect', () => {
+        userSocketIDs.delete('user._id.toString()')
         console.log('user disconnected')
     })
 })
@@ -47,3 +75,5 @@ io.on('connection', socket => {
 app.use(errorMiddleware)
 
 server.listen(port, () => console.log(`Server running on port ${port} in ${envMode} mode`))
+
+export { envMode, adminKey, userSocketIDs }
--- server/lib/helper.js
+++ server/lib/helper.js
@@ -1,1 +1,5 @@
+import { userSocketIDs } from "../app.js"
+
 export const findOtherPerson = (members, id) => members.find(m => m._id.toString() !== id.toString())
+
+export const getSockets = (users = []) => users.map(u => userSocketIDs.get(u._id.toString()))
