7f04bf406c8b7bc8110098c3e2b6039027dfb971
Supradeep <supradeep2004@gmail.com>
2024-04-27 19:28:30 +0530

    Not Found Page

--- client/src/pages/Error404.jsx
+++ client/src/pages/Error404.jsx
@@ -1,10 +1,20 @@
-import {} from 'react'
+import { Error } from '@mui/icons-material'
+import { Container, Stack, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 const Error404 = () => {
   return (
-    <div>
-        404
-    </div>
+    <Container className='h-screen w-screen'>
+      <Stack className='items-center gap-8 content-center'>
+        <Error />
+        <Typography variant='h1' fontSize='10rem'>
+          404 Not Found
+        </Typography>
+        <Link to='/'>
+          Go to Home
+        </Link>
+      </Stack>
+    </Container>
   )
 }
 
